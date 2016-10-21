3585a3b5bbd8837b1d2a039a5fb064bddc3b211d
Giff Song <giff.song@gmail.com>
2016-10-21 21:02:31 +0900

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+declare module 'gl-matrix/src/gl-matrix/vec2' {
+  import { vec2 } from 'gl-matrix';
+  export = vec2;
+}
+
+declare module 'gl-matrix/src/gl-matrix/vec3' {
+  import { vec3 } from 'gl-matrix';
+  export = vec3;
+}
+
+declare module 'gl-matrix/src/gl-matrix/mat3' {
+  import { mat3 } from 'gl-matrix';
+  export = mat3;
+}
+
+declare module 'gl-matrix/src/gl-matrix/mat4' {
+  import { mat4 } from 'gl-matrix';
+  export = mat4;
+}
+
+declare module 'gl-matrix/src/gl-matrix/quat' {
+  import { quat } from 'gl-matrix';
+  export = quat;
+}
